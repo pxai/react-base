c19576bcdf43c021f36ec22c3fda7f8b6a5e43c3
pxai <pello.altadill@gmail.com>
2021-05-25 14:58:18 +0200

    Move context to base

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import Panel from './Panel';
-import { AppContext, defaultContext } from './AppContext';
+import FakeServer from './FakeServer';
+import EventEmitter from 'eventemitter3';
+import { AppContext } from './AppContext';
+
+const appContext = {
+  title: 'Superprogram v1.0', 
+  color: 'red',
+  server: new FakeServer(),
+  bus: new EventEmitter()
+};
 
 function App() {
   return (
     <div className="App">
-      <AppContext.Provider value={defaultContext}>
+      <AppContext.Provider value={appContext}>
         <header>
         Header
         </header>
